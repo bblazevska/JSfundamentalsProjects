046b638710facf7de1555b52b71094fd65d76d22
kuzeva <kuzeva01@gmail.com>
2021-08-20 11:06:12 +0200

    arrays + chalenge #2

--- part 2/challenges.js
+++ part 2/challenges.js
@@ -1,22 +1,40 @@
 // --------- CHALLENGE #1 ----- //
 
-const calcAverage = (score1, score2, score3) => {
-   return (score1 + score2 + score3) / 3;
+// const calcAverage = (score1, score2, score3) => {
+//    return (score1 + score2 + score3) / 3;
+// }
+
+// function checkWinner (avgDolphins, avgKoalas){
+//    if(avgDolphins >= 2*avgKoalas){
+//       console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);  
+//    }else if(avgKoalas >= 2*avgDolphins){
+//       console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
+//    }else console.log('There is no winner!');
+// }
+
+// const avgDolphins1 = calcAverage (44,23,71);
+// const avgDolphins2 = calcAverage (85,54,41);
+
+// const avgKoalas1 = calcAverage(65,54,49);
+// const avgKoalas2 = calcAverage(23,34,27);
+
+// checkWinner(avgDolphins1,avgKoalas1);
+// checkWinner(avgDolphins2,avgKoalas2);
+
+
+// --------- CHALLENGE #2 ----- //
+
+const calcTip = (bill) => {
+   return bill <=300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 }
 
-function checkWinner (avgDolphins, avgKoalas){
-   if(avgDolphins >= 2*avgKoalas){
-      console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);  
-   }else if(avgKoalas >= 2*avgDolphins){
-      console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`);
-   }else console.log('There is no winner!');
-}
+// console.log(calcTip(100));
 
-const avgDolphins1 = calcAverage (44,23,71);
-const avgDolphins2 = calcAverage (85,54,41);
+const bills = [125, 555, 44];
 
-const avgKoalas1 = calcAverage(65,54,49);
-const avgKoalas2 = calcAverage(23,34,27);
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
 
-checkWinner(avgDolphins1,avgKoalas1);
-checkWinner(avgDolphins2,avgKoalas2);
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
--- part 2/script.js
+++ part 2/script.js
@@ -48,3 +48,69 @@
 
 // console.log(yearsUntillRetirement(1998, 'Bojana'));
 // console.log(yearsUntillRetirement(1990, 'Bob'));
+
+
+//-------- ARRAYS --------------//
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// console.log(friends);
+
+// const years = new Array(1991, 1984, 2008, 2020);
+// console.log(years);
+
+// console.log(friends[0]);
+// console.log(friends[2]);
+
+// console.log(friends.length);
+
+// friends[2] = 'Jay';
+// console.log(friends);
+
+// // exercise
+
+// const calcAge = function (birthYear){
+//    return 2037 - birthYear;
+// }
+
+// const years = [1990, 1967, 2002, 2010, 2018];
+
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+
+// console.log(age1, age2, age3);
+
+// const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+
+// console.log(ages);
+
+
+// -- ARRAY METHODS -- //
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay');
+// console.log(friends);
+
+// friends.unshift('John');
+// console.log(friends);
+
+
+// const popped = friends.pop();
+// console.log(friends);
+// console.log(popped);
+
+// friends.shift();
+// console.log(friends);
+
+
+// console.log(friends.indexOf('Steven'));
+// console.log(friends.indexOf('Bob'));
+
+
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Bob'));
+
+// if(friends.includes('Steven')){
+//    console.log('You have a friend called Steven.');
+// }
